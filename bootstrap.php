3253896b350696ee7ab6a431c21a42525ba50037
Markus Fischer <markus@fischer.name>
2014-10-13 07:43:59 +0200

    composer: fix autoloading by trying multiple locations

--- bootstrap.php
+++ bootstrap.php
@@ -24,6 +24,22 @@
  */
 use Mfn\PHP\Reflection\Generator\Util;
 
-require_once __DIR__ . '/vendor/autoload.php';
+# Try to locate composer autoloader
+$foundAutoload = false;
+foreach (['/../../autoload.php', '/vendor/autoload.php'] as $relDir) {
+  if (file_exists(__DIR__ . $relDir)) {
+    require __DIR__ . $relDir;
+    $foundAutoload = true;
+    break;
+  }
+}
+
+if (!$foundAutoload) {
+  echo 'Unable to find composer infrastructure, please see' . PHP_EOL;
+  echo 'https://github.com/mfn/php-reflection-gen for installation.' . PHP_EOL;
+  exit(1);
+}
+
+unset($relDir, $foundAutoload);
 error_reporting(E_ALL);
 Util::installMinimalError2ExceptionHandler();
