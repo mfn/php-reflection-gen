babc39c7f4eccb084f8487cbfe66d11832438caf
Markus Fischer <markus@fischer.name>
2018-11-25 19:25:14 +0100

    Add strict_types

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 use Mfn\PHP\Reflection\Generator\Util;
 
--- lib/Console/Generator.php
+++ lib/Console/Generator.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Mfn\PHP\Reflection\Generator\Console;
 
